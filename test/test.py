6693ff9d2c03a412e8585001f0a2e6217edbbb43
Josh Jeppson <joshua.jeppson@usu.edu>
2022-02-09 17:25:59 -0700

    Added example of format string

--- test/test.py
+++ test/test.py
@@ -6,5 +6,10 @@ def setup():
     os.system('javac ../src/scaffoldImportanceSampling.java')
 
 def createPrismFile(numStates = 10):
+    '''
+Creates a prism file of a number of states (input) and returns the path of the file made
+    '''
+    filename = f"prismFileTmp_{numStates}_states.sm"
     # TODO: make the prism model generator take the number of states as a command line argument
-    os.system(f"java ../src/PrismModelGenerator ")
+    os.system(f"java ../src/PrismModelGenerator {numStates} > {filename} ")
+    return filename
