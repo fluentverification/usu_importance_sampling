043a8637c00a9861c0b703163d5ed2883b142ded
BlueJayCash <62619619+BlueJayCash@users.noreply.github.com>
2022-03-09 11:36:02 -0700

    Updated Generator to include seed path and 0 paths

--- src/PrismModelGenerator.java
+++ src/PrismModelGenerator.java
@@ -5,15 +5,48 @@ public class PrismModelGenerator {
     public static void main(String[] args) {
 
 
-        int numberOfStates = Integer.parseInt(args[1]);
+        Scanner input1 = new Scanner(System.in);
+        System.out.print("Enter the number of States: ");
+        int numberOfStates = input1.nextInt();
+        Scanner input2 = new Scanner(System.in);
+        System.out.print("Enter the length of seed path (Length must be less than number of states): ");
+        int lengthOfSeedPath = input2.nextInt();
         double maxRate = 10;
 
+        boolean[][] truthArray = new  boolean[numberOfStates][numberOfStates];
+        double[][] probArray = new double[numberOfStates][numberOfStates]; //Array of all state probabilities//
+
         System.out.println("ctmc");
-        System.out.println();
+        System.out.println("\n    //This section contains the seed path probabilities//");
+
+        int startState = 0;
+        for (int count = 0; count < lengthOfSeedPath; count++) {
+            int randomNumber = (int) (numberOfStates * Math.random());
+            if(startState != randomNumber) {
+                double probRandom = (maxRate * Math.random());
+                System.out.printf("    const double R" + startState + "_" + randomNumber + " = " + "%.3f" + ";\n", probRandom);
+                truthArray[startState][randomNumber] = true;
+                probArray[startState][randomNumber] = probRandom;
+            }
+            startState = randomNumber;
+
+        }
+        System.out.println("    //End of seed path section//\n");
+
         for (int count = 0; count < numberOfStates; count++) {
             for (int i = 0; i < numberOfStates; i++) {
                 if(count != i) {
-                    System.out.printf("    const double R" + count + "_" + i + " = " + "%.3f" + ";\n", (maxRate * Math.random()));
+                    if(!truthArray[count][i]) {
+                        if(Math.random() < 0.75) {
+                            double probRandom = (maxRate * Math.random());
+                            System.out.printf("    const double R" + count + "_" + i + " = " + "%.3f" + ";\n", probRandom);
+                            probArray[count][i] = probRandom;
+                        }
+                        else{
+                            System.out.printf("    const double R" + count + "_" + i + " = " + "%.3f" + ";\n", 0.000);
+                            probArray[count][i] = 0.000;
+                        }
+                    }
                 }
             }
         }
@@ -42,3 +75,4 @@ public class PrismModelGenerator {
         System.out.println();
     }
 }
+
