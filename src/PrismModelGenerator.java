df242bd3cf72d5b2b962ebe8d9d5b8fce77dd86d
BlueJayCash <62619619+BlueJayCash@users.noreply.github.com>
2022-03-23 16:55:02 -0600

    Updated PrismModelGenerator for percent of connect

--- src/PrismModelGenerator.java
+++ src/PrismModelGenerator.java
@@ -11,25 +11,37 @@ public class PrismModelGenerator {
         Scanner input2 = new Scanner(System.in);
         System.out.print("Enter the length of seed path (Length must be less than number of states): ");
         int lengthOfSeedPath = input2.nextInt();
-        double maxRate = 10;
+        Scanner input3 = new Scanner(System.in);
+        System.out.print("Enter the connectivity of state space (Percentage): ");
+        float connectivity = input3.nextFloat();
+
+        double maxRate = 0.1;
 
         boolean[][] truthArray = new  boolean[numberOfStates][numberOfStates];
-        double[][] probArray = new double[numberOfStates][numberOfStates]; //Array of all state probabilities//
+        boolean[] seedPathArray = new boolean[numberOfStates]; //Array of all state probabilities//
 
-        System.out.println("ctmc");
+        System.out.println("\nctmc");
         System.out.println("\n    //This section contains the seed path probabilities//");
 
         int startState = 0;
-        for (int count = 0; count < lengthOfSeedPath; count++) {
-            int randomNumber = (int) (numberOfStates * Math.random());
-            if(startState != randomNumber) {
-                double probRandom = (maxRate * Math.random());
-                System.out.printf("    const double R" + startState + "_" + randomNumber + " = " + "%.3f" + ";\n", probRandom);
-                truthArray[startState][randomNumber] = true;
-                probArray[startState][randomNumber] = probRandom;
-            }
-            startState = randomNumber;
 
+        for (int count = 0; count < lengthOfSeedPath; count++) { //Creates seed path of probabilities//
+            int randomNumber = (int) (numberOfStates * Math.random()); //Random state to connect to//
+            if(startState != randomNumber && !seedPathArray[randomNumber]) {
+                double probRandom = (maxRate * Math.random()); //Probability of the new connection//
+                System.out.printf("    const double R" + startState + "_" + randomNumber + " = " + "%.3f" + ";\n", probRandom); //Prints out connection to model//
+                truthArray[startState][randomNumber] = true; //Prevents connection from showing up later in the model//
+                seedPathArray[randomNumber] = true; //Prevent the seed path from returning to a past state//
+
+                startState = randomNumber; //Sets the end state to the new start state//
+
+                if(!seedPathArray[0]){ //Makes sure that the seed path cannot return to 0 after it has left the state of 0//
+                    seedPathArray[0] = true;
+                }
+            }
+            else{ //Causes the seed path to generate until it reaches the desired length//
+                count--;
+            }
         }
         System.out.println("    //End of seed path section//\n");
 
@@ -37,14 +49,12 @@ public class PrismModelGenerator {
             for (int i = 0; i < numberOfStates; i++) {
                 if(count != i) {
                     if(!truthArray[count][i]) {
-                        if(Math.random() < 0.75) {
+                        if(Math.random() < connectivity * 0.01) {
                             double probRandom = (maxRate * Math.random());
                             System.out.printf("    const double R" + count + "_" + i + " = " + "%.3f" + ";\n", probRandom);
-                            probArray[count][i] = probRandom;
                         }
                         else{
-                            System.out.printf("    const double R" + count + "_" + i + " = " + "%.3f" + ";\n", 0.000);
-                            probArray[count][i] = 0.000;
+                            //System.out.printf("    const double R" + count + "_" + i + " = " + "%.3f" + ";\n", 0.000);
                         }
                     }
                 }
@@ -75,4 +85,3 @@ public class PrismModelGenerator {
         System.out.println();
     }
 }
-
