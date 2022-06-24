2b021c5b5978e48a7bc6e64cfe0ae6a72146fee5
gerbs-11 <andrewj.gerber@gmail.com>
2022-06-24 11:04:47 -0600

    Histogram definition complete. Added ProbabilityDistribution interface

---
+++ src/ProbabilityDistribution.java
@@ -0,0 +1,31 @@
+public interface ProbabilityDistribution {
+
+    /**
+     * Returns the probability of the given value
+     * @param value value to get probability for
+     * @return probability associated with the value
+     */
+    public double getProbability(double value);
+
+    /**
+     * Returns the probability  of the given value. The tolerance
+     * is used to compensate for rounding errors. In the case of
+     * multiple values within the tolerance, the lowest value is
+     * used.
+     * 
+     * This only makes sense to use with discrete distributions
+     * 
+     * @param value value to get probability for
+     * @param tolerance tolerance +/- of value
+     * @return probability associated with the value
+     */
+    public double getProbability(double value, double tolerance);
+
+    /**
+     * Returns a random value based on this probability
+     * distribution
+     * @return random value from this distribution
+     */
+    public double random();
+    
+}
