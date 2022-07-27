edf271ee8ad78fea465cf8d219f4d0ec6c6a3986
Eric Reiss <reiss_eric@hotmail.com>
2022-07-27 12:44:44 -0600

    Added module and variable declarations. Fixed arrow.

--- src/main/java/imsam/SparseModelGenerator.java
+++ src/main/java/imsam/SparseModelGenerator.java
@@ -230,9 +230,11 @@ public class SparseModelGenerator implements Callable<Integer> {
         String filename = resolvePlaceholders(outputFilename, iteration+1);
         logger.debug("Output filename after resolving placeholders: '" + filename + "'");
         FileWriter writer = new FileWriter(filename);
-        writer.write("ctmc\n");
+        writer.write("ctmc\n\n");
+        writer.write("module M1\n");
+        writer.write("x : [0.."+(numberOfStates-1)+"];\n");
         for (int i=0; i<numberOfStates; i++) {
-            writer.write("[] x=" + i + " - > ");
+            writer.write("[] x=" + i + " -> ");
             for (int i2=0; i2<stateSpace[i].transitionsOut.size(); i2++) {
                 TransitionPath transition = stateSpace[i].transitionsOut.get(i2);
                 writer.write((int) transition.rate + ":(x'=" + transition.end + ")");
@@ -243,6 +245,7 @@ public class SparseModelGenerator implements Callable<Integer> {
                 }
             }
         }
+        writer.write("endmodule");
         writer.close();
     }
 
