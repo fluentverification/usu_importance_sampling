e96cd60c8a78a684703ae44dab3c1d298b03b295
Eric Reiss <reiss_eric@hotmail.com>
2022-08-03 18:56:36 -0600

    Add option for target state list, change some debug statements

--- src/main/java/imsam/mgen/TerminatingPathModelGenerator.java
+++ src/main/java/imsam/mgen/TerminatingPathModelGenerator.java
@@ -3,6 +3,11 @@ package imsam.mgen;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.message.ExitMessage;
+import org.json.JSONObject;
+import org.kohsuke.args4j.Option;
+
+
 /**
  * This is a model generator that will accept a list of target states and force all paths to converge on one of said states
  * 
@@ -12,6 +17,51 @@ import java.util.List;
  * @author Eric Reiss
  */
 public class TerminatingPathModelGenerator extends MGen{
+    public List<Integer> targets;
+    
+    @Option(name="--target-list",usage="In a space-separated String, list states that all paths will converge to. default: generates random targets")
+    public String targetList = "";
+
+    //Parse config file options
+    @Override 
+    protected boolean parseSubclassConfigParam(JSONObject json, String key){
+        if(key == "targetList"){
+            if(targetList.isBlank()){
+                targetList = json.getString(key);
+            }
+            return true;
+        }else
+            return false;
+    }
+
+    //Set default options
+    @Override
+    protected void initSubclassParamDefaults(){
+        logger.debug("Initializing Subclass Parameter Defaults");
+        if(targetList.isBlank()){
+            logger.debug("target-list is blank, generating target states");
+            targets = generateTargetStates();
+        }else{
+            logger.debug("target list is "+ targetList);
+            String[] parsedTargetList = targetList.split(" ");
+            targets = new ArrayList<>();
+            for(String targetString : parsedTargetList){
+                try{
+                    logger.debug("adding "+ Integer.parseInt(targetString)+ " to target ArrayList");
+                    targets.add(Integer.parseInt(targetString));
+                    
+                }catch(NumberFormatException e){
+                    String errMsg = "The target state list must be made up of integers separated by a space. "+
+                    "Ex)\"1 5 9 12\"";
+                    logger.error(errMsg);
+                    System.exit(1);
+                }
+            }
+        }
+    }
+
+   
+
     /**
      * Generate model by creating path from target state to initial 
      * then ensuring every path converges to target state path
@@ -26,11 +76,10 @@ public class TerminatingPathModelGenerator extends MGen{
         }
 
         //Create path from initial to target
-        logger.debug("Generating target path");
+        logger.debug("Generating target path(s)");
         boolean[] onTargetPath = new boolean[numberOfStates];
-        List<Integer> targets = generateTargetStates();
-
         for(int targetIndex=0; targetIndex<targets.size(); targetIndex++){
+            logger.trace("generating target path for "+ targets.get(targetIndex));
             int target = targets.get(targetIndex);
             onTargetPath[target] = true;
             int current = target;
@@ -58,6 +107,7 @@ public class TerminatingPathModelGenerator extends MGen{
             for(int transitionId = 0; transitionId<transitionCount; transitionId++){
                 int successor = (int)(Math.random()*numberOfStates);
                 if(stateId != successor && !targets.contains(stateId)){
+                    logger.trace("Connecting states "+ stateId + " and "+ successor);
                     TransitionPath transition = new TransitionPath(stateId, successor, transitionRateDistribution.random());
                     stateSpace[stateId].transitionsOut.add(transition);
                     stateSpace[successor].transitionsIn.add(transition);
@@ -96,23 +146,26 @@ public class TerminatingPathModelGenerator extends MGen{
             }
         }
     }
-        /**
-         * Randomly choose target states from state space, bounded at 1/3 of state space
-         * @return List of target states
-         */
-        private List<Integer> generateTargetStates(){
+    /**
+     * Randomly choose target states from state space, arbitrarily bounded at 1/3 of state space
+     * @return List of target states
+     */
+    private List<Integer> generateTargetStates(){
+        StringBuilder strbld = new StringBuilder();
         List<Integer> targets = new ArrayList<>();
         double third = 0.3333333333333333f;
         int targetBound = (int)(third*numberOfStates); //get bound targets to 1/3 of state space
-        logger.info("Targetbound "+targetBound);
+        logger.info("Target Bound: "+targetBound);
         int numberOfTargets = (int)((Math.random()*(targetBound-1))+1); //Randomly choose number of targets, always ensuring at least 1
-        logger.info("TargetSize "+numberOfTargets);
-        logger.debug(numberOfTargets+" have been selected as target states");
+        logger.info("Target Size: "+numberOfTargets);
+        logger.debug(numberOfTargets+(numberOfTargets==1?" state has ":" states have ")+"been selected as target(s)");
         for(int targetIndex = 0; targetIndex < numberOfTargets; targetIndex++){
             int pickTarget = (int)((Math.random()*(numberOfStates-1))+1); //draw targets
             targets.add(pickTarget);
             logger.debug("Picking state "+pickTarget+" as a target state");
+            strbld.append(pickTarget+" ");
         }
+        logger.info("Generated Target States: "+ strbld.toString());
         return targets;
     }
 }
